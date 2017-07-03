9a438b93f5e9ebeff30f5de45d103a1e7a2ecb6e
Adrian Edwards <DeveloperACE@users.noreply.github.com>
2017-07-03 12:32:06 -0700

    change to un-depreceated method `.setPlayingMessage`. bump version number and some misc project file updates

--- src/main/java/Events.java
+++ src/main/java/Events.java
@@ -1,6 +1,5 @@
 import sx.blah.discord.api.events.EventSubscriber;
 import sx.blah.discord.handle.impl.events.ReadyEvent;
-import sx.blah.discord.handle.obj.Status;
 
 /**
  * Sets bots playing text to
@@ -8,6 +7,6 @@ import sx.blah.discord.handle.obj.Status;
 public class Events {
     @EventSubscriber
     public void onReadyEvent(ReadyEvent event) {
-        event.getClient().changeStatus(Status.game("with code."));  // Fun message to show bot is online and working.
+        event.getClient().changePlayingText("with code.");  // Fun message to show bot is online and working.
     }
 }
